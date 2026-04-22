b5b0fc511191a8a81ee34230fdfa4f94c1695451
Dev <dev@Devs-MacBook-Pro.local>
2026-04-22 18:57:55 +0500

    Started DB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'auth_system',
+      autoLoadEntities: true,
+      synchronize: true, // ⚠️ only for dev
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
