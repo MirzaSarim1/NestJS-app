eac190573c23dca3fcb7c51d91d8d1085f05e759
Dev <dev@Devs-MacBook-Pro.local>
2026-04-22 20:03:29 +0500

    Login API done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,54 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
     async register(username: string, password: string) {
         const existingUser = await this.usersService.findByUsername(username);
 
         if (existingUser) {
-        throw new ConflictException('User already exists');
+            throw new ConflictException('User already exists');
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await this.usersService.create({
-        username,
-        password: hashedPassword,
+            username,
+            password: hashedPassword,
         });
 
         return {
             id: user.id,
             username: user.username,
+        };
+    }
+
+    async login(username: string, password: string) {
+        const user = await this.usersService.findByUsername(username);
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = {
+            sub: user.id,
+            username: user.username,
+            role: user.role,
+        };
+
+        return {
+            access_token: this.jwtService.sign(payload),
         };
     }
 }
