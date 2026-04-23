b4c00c016387946470b860f9167b9ea06f3718e1
Dev <dev@Devs-MacBook-Pro.local>
2026-04-23 16:18:04 +0500

    Custom decorator GetUser done

---
+++ src/users/interfaces/jwtUser.interface.ts
@@ -0,0 +1,5 @@
+export interface JwtUser {
+    id: number,
+    username: string,
+    role: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import type { JwtUser } from './interfaces/jwtUser.interface';
 
 @Controller('users')
 export class UsersController {
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    getProfile() {
-        return { message: 'You are authenticated' };
+    getProfile(@GetUser() user: JwtUser) {
+        return user;
     }
 }
